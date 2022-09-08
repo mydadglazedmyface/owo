a15754efd4f3401d699960a5c67a908a7f42f7e6
greeee <root@LAPTOP-K2SV7G9A.localdomain>
2022-09-08 16:53:17 +0900

    help

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: npongdon <npongdon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 18:17:21 by npongdon          #+#    #+#             */
-/*   Updated: 2022/09/05 20:08:25 by npongdon         ###   ########.fr       */
+/*   Updated: 2022/09/07 19:13:04 by npongdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (s3 != 0)
 	{
-		ft_strlcpy(s3, s1, ft_strlen(s1));
-		ft_strlcpy(s3 + ft_strlen(s1), s2, ft_strlen(s2));
+		ft_strlcpy(s3, s1, ft_strlen(s1) + 1);
+		ft_strlcpy(s3 + ft_strlen(s1), s2, ft_strlen(s2) + 1);
 		s3[ft_strlen(s3)] = '\0';
 	}
 	return (s3);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*a = "123dwsdw";
-	char	*b = "vrvrvrvroooom";
-	char	*c = ft_strjoin(a, b);
-	puts(c);
-	free(s);
-	return (0);
 }
